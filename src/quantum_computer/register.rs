0234a8c913c7e9c20286aebb40483d45a31739bf
Sean Chen <seanchen11235@gmail.com>
2021-04-27 17:13:05 -0500

    Impl ClassicalRegister

--- src/quantum_computer/register.rs
+++ src/quantum_computer/register.rs
@@ -1,4 +1,32 @@
+use std::fmt;
+
+#[derive(Clone, Debug)]
 pub struct ClassicalRegister {
-    value: bool,
+    capacity: usize,
+    values: Vec<bool>,
 }
 
+impl fmt::Display for ClassicalRegister {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.capacity == 1 {
+            write!(f, "{}", self.values[0])
+        } else {
+            write!(f, "{:?}", self.values)
+        }
+    }
+}
+
+impl Default for ClassicalRegister {
+    fn default() -> Self {
+        Self::new(1)
+    }
+}
+
+impl ClassicalRegister {
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            capacity,
+            values: vec![false; capacity],
+        }
+    }
+}
