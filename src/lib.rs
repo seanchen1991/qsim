b815271e08a0d96c3d31076d9593e0e0a803b969
Sean Chen <seanchen11235@gmail.com>
2021-04-27 13:28:21 -0500

    Construct zero basis and hadamard gate from functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,26 @@
-use ndarray::{self, Array2};
-
-/// The |0> computational basis state.
-const KET_0: &[&[i32]] = &[&[1], &[0]];
+use ndarray::{arr2, Array2};
+use num_complex::Complex32;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+/// Returns the |0> computational basis state in matrix form.
+pub fn zero_basis() -> Array2<Complex32> {
+    arr2(&[
+        [Complex32::new(1.0, 0.0)],
+        [Complex32::new(0.0, 0.0)],
+    ])
+}
+
+/// Returns the Hadamard gate in matrix form.
+pub fn hadamard() -> Array2<Complex32> {
+    let sqrt2 = 2.0_f32.sqrt();
+    let mut gate = arr2(&[
+        [Complex32::new(1.0, 0.0), Complex32::new(1.0, 0.0)],
+        [Complex32::new(1.0, 0.0), Complex32::new(-1.0, 0.0)],
+    ]);
+
+    gate.map_mut(|c| c.unscale(sqrt2))
+}
 
 /// An interface for a qubit
 pub trait Qubit {
