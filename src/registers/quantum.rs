09fa7bde387de82b9c58cadbaf861501d0e46a50
Sean Chen <seanchen11235@gmail.com>
2021-04-28 18:30:35 -0500

    Stub out QuantumRegister type

--- src/registers/quantum.rs
+++ src/registers/quantum.rs
@@ -1,7 +1,27 @@
+use super::ClassicalRegister;
+
 use crate::key::Ket;
 
+#[derive(Debug, Clone)]
 pub struct QuantumRegister {
     width: usize,
     collapsed: bool,
     ket: Ket,
+}
+
+impl QuantumRegister {
+    /// Initializes a new QuantumRegister with `capacity` qubits.
+    pub fn new(capacity: usize) -> Self {
+        todo!();
+    }
+
+    /// Collapses the QuantumRegister into a ClassicalRegister.
+    pub fn measure(&mut self) -> ClassicalRegister {
+        todo!();
+    }
+
+    /// Applies the given gate to the state of the QuantumRegister.
+    pub fn apply(&mut self, gate: Gate) {
+        todo!();
+    }
 }
