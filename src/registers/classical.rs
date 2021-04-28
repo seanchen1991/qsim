d7ad54d7d1870f1b92d67f06c7c2b23c68b9dc7a
Sean Chen <seanchen11235@gmail.com>
2021-04-28 14:21:07 -0500

    Remove TryInto usage

--- src/registers/classical.rs
+++ src/registers/classical.rs
@@ -5,7 +5,6 @@ extern crate quickcheck;
 extern crate quickcheck_macros;
 
 use std::fmt;
-use std::convert::TryInto;
 use byteorder::{ByteOrder, BigEndian};
 
 pub const REGISTER_SIZE: usize = 4;
@@ -29,15 +28,13 @@ impl From<u32> for ClassicalRegister {
     fn from(n: u32) -> Self {
         let mut values = [0; 4];
         BigEndian::write_u32(&mut values, n);
-
         Self(values)
     }
 }
 
 impl From<&ClassicalRegister> for u32 {
     fn from(c: &ClassicalRegister) -> Self {
-        let values = c.0.clone();
-        u32::from_be_bytes(values.try_into().expect("Failed to convert ClassicalRegister to u32"))
+        BigEndian::read_u32(&c.0)
     }
 }
 
